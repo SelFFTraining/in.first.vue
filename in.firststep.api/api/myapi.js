78322c3404252d7420a502c6b0418ab6369f1dbb
sjalagar <sjalagar@SJALAGAR-IN.in.oracle.com>
2019-11-15 10:43:00 +0530

    adding in.firsstep.api

---
+++ in.firststep.api/api/myapi.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const path = require('path');
+const PUBLIC_PATH = path.join(__dirname, '../public/dist');
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.json())
+app.use(express.static(PUBLIC_PATH))
+
+let taskList = {tasks: []};
+
+app.get('/api/task', (req, res) =>{
+    res.send(taskList);
+});
+
+app.post('/api/task', (req, res) =>{
+    taskList.tasks.push(req.body);
+    res.status(201).send(req.body);
+});
+app.del('/api/task', (req, res)=>{
+    console.log(req.body);
+    if(!req.body.title) {
+        res.status(422).send('require title query string')
+    }
+    taskList.tasks = taskList.tasks.filter(data=>{
+        return data.data.title !== req.body.title
+    });
+    res.send(taskList);
+})
+
+app.listen(PORT, ()=>{
+    console.log('started listening at ' +PORT);
+});
+
+
+
